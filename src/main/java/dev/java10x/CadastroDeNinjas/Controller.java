cb81449b54fc87e73fb24905bedc6ef99b631bac
evertonlsds <everton0102@gmail.com>
2026-05-14 22:06:21 -0300

    mapping change

--- src/main/java/dev/java10x/CadastroDeNinjas/Controller.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Controller.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping
 public class Controller {
 
-    @GetMapping("/boasvindas")
+    @GetMapping("/boavinda")
     public String boasVindas(){
         return" Essa é minha primeira mensagem nessa rota";
     }
