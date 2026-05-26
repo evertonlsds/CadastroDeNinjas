dc3659b9fa65b396075aab81890f3437cce6d699
evertonlsds <everton0102@gmail.com>
2026-05-26 20:24:41 -0300

    fix: Coloca a coluna de email como UNIQUE

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -19,9 +19,14 @@ public class NinjaModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
     private String nome;
+
+    @Column(unique = true)
     private String email;
+
     private int idade;
+
     @ManyToOne
     @JoinColumn(name = "missoes_id") //chave estrangeira
     private MissoesModel missoes;
